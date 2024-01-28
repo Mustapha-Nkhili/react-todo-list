37be0f408a12d911c7aab252e52c8225063fb76e
Mustapha-Nkhili <mustapha.developer.23@gmail.com>
2024-01-28 23:26:02 +0100

    Enable Enter key press to trigger Todo addition

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { addTodo } from "../actions";
 import { useDispatch } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -7,11 +7,40 @@ import { faAdd } from "@fortawesome/free-solid-svg-icons";
 export default function AddTodo() {
   const inputRef = useRef();
   const dispatch = useDispatch();
+  const addBtnRef = useRef();
 
   function handleAddTodo() {
     dispatch(addTodo(inputRef.current.value));
     inputRef.current.value = "";
   }
+
+  const handleKeyPress = (event) => {
+    // Check if the pressed key is "Enter" (key code 13)
+    if (event.key === "Enter") {
+      event.preventDefault();
+      // Simulate a click on the add button
+      if (addBtnRef.current) {
+        addBtnRef.current.click();
+      }
+    }
+  };
+
+  useEffect(() => {
+    const inputElement = inputRef.current;
+    const addBtnElement = addBtnRef.current;
+
+    // Attach the event listener to the input element when the component mounts
+    if (inputElement && addBtnElement) {
+      inputElement.addEventListener("keydown", handleKeyPress);
+    }
+
+    // Remove the event listener when the component unmounts
+    return () => {
+      if (inputElement) {
+        inputElement.removeEventListener("keydown", handleKeyPress);
+      }
+    };
+  }, [inputRef]);
 
   return (
     <div className="add-todo">
@@ -21,7 +50,11 @@ export default function AddTodo() {
         ref={inputRef}
         placeholder="Enter what you want todo"
       />
-      <button onClick={handleAddTodo} className="add-todo-btn btn">
+      <button
+        onClick={handleAddTodo}
+        className="add-todo-btn btn"
+        ref={addBtnRef}
+      >
         <FontAwesomeIcon icon={faAdd} />
       </button>
     </div>
