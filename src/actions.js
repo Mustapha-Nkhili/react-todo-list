e5ec7d32d33467604ff553b809ef275c42ba657f
Mustapha-Nkhili <mustapha.developer.23@gmail.com>
2024-01-29 18:36:37 +0100

    Refactor action creators for conciseness and readability

--- src/actions.js
+++ src/actions.js
@@ -1,42 +1,29 @@
-export const deleteTodo = (id) => {
-  return {
-    type: "todos/deleteTodo",
-    id: id,
-  };
-};
+export const deleteTodo = (id) => ({
+  type: "todos/deleteTodo",
+  id: id,
+});
 
-export const addTodo = (text) => {
-  return {
-    type: "todos/addTodo",
-    text: text,
-  };
-};
+export const addTodo = (text) => ({
+  type: "todos/addTodo",
+  text: text,
+});
 
-export const editTodo = (id, text) => {
-  return {
-    type: "todos/editTodo",
-    id: id,
-    modifiedText: text,
-  };
-};
+export const editTodo = (id, text) => ({
+  type: "todos/editTodo",
+  id: id,
+  modifiedText: text,
+});
 
-export const toggleTodoStatus = (id) => {
-  return {
-    type: "todos/toggleTodoStatus",
-    id: id
-  }
-}
+export const toggleTodoStatus = (id) => ({
+  type: "todos/toggleTodoStatus",
+  id: id,
+});
 
+export const addStatusFilter = (status) => ({
+  type: "filters/changeTodosFilterStatus",
+  status: status,
+});
 
-export const addStatusFilter = (status) => {
-  return {
-    type: "filters/changeTodosFilterStatus",
-    status: status,
-  }
-}
-
-export const addColorFilter = (color) => {
-  return {
-    type: 'filters/changeTodosFilterColor'
-  }
-}
+export const addColorFilter = (color) => ({
+  type: "filters/changeTodosFilterColor",
+});
